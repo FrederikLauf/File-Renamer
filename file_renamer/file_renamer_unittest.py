35e6445dc326ed018a8866637009627c42c64629
Frederik Lauf <frederik.lauf@gmx.net>
2025-06-08 13:15:10 +0200

    update unittest

--- file_renamer/file_renamer_unittest.py
+++ file_renamer/file_renamer_unittest.py
@@ -1,59 +1,52 @@
 # -*- coding: utf-8 -*-
 
+import unittest
 from file_renamer import FileRenamer
-from unittest_data.unit_test_result import result_name_sort, result_date_sort, result_homonymity_date_sort
-from unittest_data.unit_test_result import result_name_sort_offset, result_date_sort_offset, result_homonymity_date_sort_offset
+from unittest_data.unittest_data import FILE_LIST, result_name_sort, result_date_sort, result_homonymity_date_sort
+from unittest_data.unittest_data import FILE_LIST_WITH_OFFSET, result_name_sort_offset, result_date_sort_offset, result_homonymity_date_sort_offset
 
 
-if __name__ == "__main__":
-    """
-    unit test
-    """
+class TestSortingMethods(unittest.TestCase):
     
-    fr = FileRenamer()
-    fr._basepath = r"unittest_data/unittest_data_1"
-    fr._make_file_list()
-    fr._sort_by_name()
-    print("_sort_by_name")
-    assert fr._file_list == result_name_sort
-    for item in fr._file_list:
-        print(item)
-    print(10 * '*')
-    fr._sort_by_date()
-    print("_sort_by_date")
-    assert fr._file_list == result_date_sort
-    for item in fr._file_list:
-        print(item)
-    print(10 * '*')
-    fr._sort_by_date_and_homonymity()
-    print("_sort_by_date_and_homonymity")
-    assert fr._file_list == result_homonymity_date_sort
-    for item in fr._file_list:
-        print(item)
-    print(10 * '*')
-    print(10 * '*')
-    print("DSC = IMG + 117s")
-    fr = FileRenamer()
-    fr._basepath = r"unittest_data/unittest_data_2"
-    fr._time_offsets = [{'identifier': 'IMG', 'seconds': 117}]
-    fr._make_file_list()
-    fr._sort_by_name()
-    print("_sort_by_name")
-    for item in fr._file_list:
-        print(item)
-    assert fr._file_list == result_name_sort_offset
-    print(10 * '*')
-    fr._sort_by_date()
-    print("_sort_by_date")
-    for item in fr._file_list:
-        print(item)
-    assert fr._file_list == result_date_sort_offset
-    print(10 * '*')
-    fr._sort_by_date_and_homonymity()
-    print("_sort_by_date_and_homonymity")
-    for item in fr._file_list:
-        print(item)
-    assert fr._file_list == result_homonymity_date_sort_offset
-    print(10 * '*')
-    print("!!!No Assertion Errors!!!")
-    print(10 * '*')
+    def test_sort_by_name(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST
+        fr._sort_by_name()
+        self.assertEqual(fr._file_list, result_name_sort)
+
+    def test_sort_by_date(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST
+        fr._sort_by_date()
+        self.assertEqual(fr._file_list, result_date_sort)
+        
+    def test_sort_by_date_and_homonymity(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST
+        fr._sort_by_date_and_homonymity()
+        self.assertEqual(fr._file_list, result_homonymity_date_sort)
+
+    def test_sort_by_name_with_offset(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST_WITH_OFFSET
+        fr._time_offsets = [{'identifier': 'IMG', 'seconds': 117}]
+        fr._sort_by_name()
+        self.assertEqual(fr._file_list, result_name_sort_offset)
+
+    def test_sort_by_date_with_offset(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST_WITH_OFFSET
+        fr._time_offsets = [{'identifier': 'IMG', 'seconds': 117}]
+        fr._sort_by_date()
+        self.assertEqual(fr._file_list, result_date_sort_offset)
+
+    def test_sort_by_date_and_homonymity_with_offset(self):
+        fr = FileRenamer()
+        fr._file_list = FILE_LIST_WITH_OFFSET
+        fr._time_offsets = [{'identifier': 'IMG', 'seconds': 117}]
+        fr._sort_by_date_and_homonymity()
+        self.assertEqual(fr._file_list, result_homonymity_date_sort_offset)
+
+
+if __name__ == '__main__':
+    unittest.main()
